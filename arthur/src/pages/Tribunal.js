789574cb9f2c3c86318a97b2af809ba0915fdb46
Gabin Mobétie <gmobetie@ensc.fr>
2024-04-29 01:15:31 +0200

    var2

--- arthur/src/pages/Tribunal.js
+++ arthur/src/pages/Tribunal.js
@@ -1,9 +1,5 @@
 import NavBar from "../components/NavBar";
 import TribunalDiv from "../components/TribunalDiv";
-import {
-  getUserData,
-} from "../services/spotify";
-import { useEffect, useState } from "react";
 
 function Tribunal() {
 
